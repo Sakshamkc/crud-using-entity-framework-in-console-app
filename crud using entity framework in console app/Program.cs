a350d2d466a3ab8c90472bb10d35150511633548
Sakshamkc <Sakshamkc10@gmail.com>
2021-10-12 09:52:46 +0545

    first commit

---
+++ crud using entity framework in console app/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud_using_entity_framework_in_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //daleydb dbs = new daleydb();
+
+
+            //daley dba = new daley();
+            //dba.Firstname = "sandip";
+            //dba.Lastname = "Subedi";
+            //dba.Phone = "9810236987";
+            //dba.Age = 22;
+            //dbs.CreateStudent(dba);
+            //Console.WriteLine("Created");
+
+            //dba.StudentId = 0;
+            //dba.Firstname = "Daley";
+            //dba.Lastname = "Subedi";
+            //dba.Phone = "9810238963";
+            //dba.Age = 24;
+            //dbs.UpdateStudent(dba);
+            //Console.WriteLine("Updated");
+
+            //List<daley> lst = dbs.GetInfo();
+            //foreach (var item in lst)
+            //{
+            //    Console.WriteLine(item.StudentId + " " + item.Firstname + " " + item.Lastname + " " + item.Phone + " " + item.Age);
+            //}
+
+            //dbs.DeleteStudent(0);
+            //Console.WriteLine("Deleted");
+            //Console.ReadLine();
+        }
+        
+    }
+}
---
+++ crud using entity framework in console app/daleydb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud_using_entity_framework_in_console_app
+{
+    public class daleydb
+    {
+        DaleyramEntities _db = new DaleyramEntities();
+        public List<daley> GetInfo()
+        {
+            return _db.daleys.ToList();
+        }
+
+        public void CreateStudent(daley da)
+        {
+            _db.daleys.Add(da);
+            _db.SaveChanges();
+        }
+        public void UpdateStudent(daley da )
+        {
+            daley db = _db.daleys.Where(a => a.StudentId == da.StudentId).FirstOrDefault();
+            db.Firstname = da.Firstname;
+            db.Lastname = da.Lastname;
+            db.Phone = da.Phone;
+            db.Age = da.Age;
+            _db.SaveChanges();
+        }
+        public void DeleteStudent(int studentId)
+        {
+            daley db = _db.daleys.Where(a => a.StudentId == studentId).FirstOrDefault();
+            
+            _db.daleys.Remove(db);
+            _db.SaveChanges();
+        }
+    }
+}
